acf0a61bea65f888147c663df45ef11515ade8be
Shiquan Yang <shiquan@4180L-150398-M.local>
2020-12-31 19:03:05 +1100

    add

--- s2s-ft/multimodalKB/models/BertEncoder.py
+++ s2s-ft/multimodalKB/models/BertEncoder.py
@@ -32,6 +32,8 @@ class BertEncoder(nn.Module):
         mask = self.gen_input_mask(input_seqs.shape[1], input_seqs.shape[0], input_lengths)
         if USE_CUDA:
             mask = mask.to(self.device)
+        t1 = input_seqs.transpose(0, 1).type(torch.LongTensor)
+        print(t1.device)
         outputs = self.bert(input_seqs.transpose(0, 1).type(torch.LongTensor), attention_mask=mask)
         last_hidden_states = outputs[0]
         hidden = torch.sum(last_hidden_states, dim=1) / max_len
