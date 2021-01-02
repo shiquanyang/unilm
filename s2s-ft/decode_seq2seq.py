0717ad803e53b9d9963f74f6515cf8b67c763609
Shiquan Yang <shiquan@4180L-150398-M.local>
2021-01-02 18:30:44 +1100

    add

--- s2s-ft/decode_seq2seq.py
+++ s2s-ft/decode_seq2seq.py
@@ -166,10 +166,10 @@ def main():
     if args.max_tgt_length >= args.max_seq_length - 2:
         raise ValueError("Maximum tgt length exceeds max seq length - 2.")
 
-    device = torch.device(
-        "cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device(
+    #     "cuda" if torch.cuda.is_available() else "cpu")
+    device = "cpu"
     print(device)
-    # device = "cpu"
     n_gpu = torch.cuda.device_count()
 
     if args.seed > 0:
